a3f6dfda75883f6fcaff814ad838fa2e4d463b35
Vinod SR <vinodsr007@gmail.com>
2021-03-27 19:33:15 +0530

    storage account name corrected

--- main.tf
+++ main.tf
@@ -36,7 +36,7 @@ resource "azurerm_subnet" "sn" {
 }
 # Create our Azure Storage Account - ACITDemoSA
 resource "azurerm_storage_account" "ACITDemoSA" {
-  name                     = "ACITDemoSA"
+  name                     = "acitdemosa2021"
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
