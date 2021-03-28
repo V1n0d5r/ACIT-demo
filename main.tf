92ec0eed5a7dc3155252097439b574c259fa7311
Vinod SR <vinodsr007@gmail.com>
2021-03-28 15:42:55 +0530

    backend local

--- main.tf
+++ main.tf
@@ -141,3 +141,7 @@ resource "azurerm_network_interface_security_group_association" "example" {
     network_interface_id      = azurerm_network_interface.vmnic.id
     network_security_group_id = azurerm_network_security_group.nsg.id
 }
+
+terraform {
+backend "local" {}
+}
