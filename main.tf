16aeeb5b5f77db29ae374420b52b5bd6d05c44c1
Vinod SR <vinodsr007@gmail.com>
2021-03-29 10:15:50 +0530

    nsg corrected

--- main.tf
+++ main.tf
@@ -117,7 +117,7 @@ resource "azurerm_network_security_group" "nsg" {
         access                     = "Allow"
         protocol                   = "Tcp"
         source_port_range          = "*"
-        destination_port_range     = "5985,5986"
+        destination_port_range     = "5985-5986"
         source_address_prefix      = "*"
         destination_address_prefix = "*"
     }
